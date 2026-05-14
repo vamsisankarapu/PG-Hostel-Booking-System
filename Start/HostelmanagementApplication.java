4d566a389c6332c0963f145a514f917269785b62
vamsisankarapu <vamsisankarapu79@gmail.com>
2026-05-14 11:56:57 +0530

    Add files via upload

---
+++ Start/HostelmanagementApplication.java
@@ -0,0 +1,13 @@
+package com.example.hostelmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HostelmanagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HostelmanagementApplication.class, args);
+	}
+
+}
